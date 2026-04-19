fff1d39df270298b90d8265dbfec3261723b83e0
JeffreyGaydos <jeff13gaydos@gmail.com>
2026-04-19 15:36:52 -0400

    WIP; doing this later

--- st-peter-custom-mods.php
+++ st-peter-custom-mods.php
@@ -36,6 +36,7 @@ function spc_settings_menu() {
 
 function load_admin_scripts( $hook ) {
     wp_enqueue_script( 'spc-admin-panel', plugins_url('/includes/js/spc_admin_panel.js', __FILE__), '', '1.1');
+    wp_enqueue_style( 'spc-admin-panel-css', plugins_url('/includes/css/spc_admin_panel.css', __FILE__), '', '1.0');
 }
 
 //Creates the html of the plugin's setting page, passing data through the backend as needed
@@ -54,13 +55,9 @@ if( !function_exists("spc_acp_page") ) {
             ?>
             <h2>Mass Schedule</h2>
             <input type="checkbox" name="spc_masstimes" <?php spc_get_checked('spc_masstimes') ?> >Turn On/Off Mass Schedule System</input>
+            <textarea type="text" name="spc_masstimes_json" style="display: none"><?php echo tec_get_text('spc_masstimes_json'); ?></textarea>
             <p>Use the dropdowns to set a mass schedule to display. The current mass schedule is listed below the dropdowns. Click the cancel checkbox on a specific mass time to cancel the mass time just for this week. Click the x button on a specific mass time to remove it forever.</p>
-            <p>Current "Default" Mass Times</p>
-            <ul>
-                <li>Sunday, 11:30 AM Every Week<input type="checkbox" name="ms-cancel">Cancel</input><button id="ms-delete">x</button></li>
-                <li>Sunday, 7:00 PM Every Week</li>
-            </ul>
-            <button>+ Add Another Day</button>
+
             <select name="ms-day-of-week">
                 <option value="-1" disabled selected>Select a day...</option>
                 <option value="Sunday">Sunday</option>
@@ -72,6 +69,33 @@ if( !function_exists("spc_acp_page") ) {
                 <option value="Saturday">Saturday</option>
             </select>
             <input type="time" id="ms-time">
+            <button>< Add This Mass Time</button>
+
+            <h3>Current Configured Mass Times</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Time</th>
+                        <th>Options</th>
+                        <th></th>
+                        <th>Is Actually Saved</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Sunday, 11:30 AM Every Week</td>
+                        <td><input type="checkbox" name="ms-cancel">Cancel for 1 Week</input></td>
+                        <td><input type="checkbox" name="ms-delete">Delete Permanently</input></td>
+                        <td id="s">Saved!</td>
+                    </tr>
+                    <tr>
+                        <td>Sunday, 7:00 PM Every Week</td>
+                        <td><input type="checkbox" name="ms-cancel">Cancel for 1 Week</input></td>
+                        <td><input type="checkbox" name="ms-delete">Delete Permanently</input></td>
+                        <td id="p">Pending Changes - Not Saved!</td>
+                    </tr>
+                </tbody>
+            </table>
             
             <h2>Custom Slider</h2>
             <input type="checkbox" name="spc_slider" <?php spc_get_checked('spc_slider') ?> >Turn On/Off Custom Slider</input>
@@ -117,5 +141,6 @@ if( !function_exists("update_spc_info") ) {
     function update_spc_info() {
         register_setting( 'spc-settings', 'spc_masstimes' );
         register_setting( 'spc-settings', 'spc_slider' );
+        register_setting( 'spc-settings', 'spc_masstimes_json');
     }
 }
