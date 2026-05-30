75c3dcf5c1a72808a777d4012b5b8e66eebcae6f
JeffreyGaydos <jeff13gaydos@gmail.com>
2026-05-29 23:30:30 -0400

    disable scroll on body when modal opens (actually this time)

--- includes/js/spc_bubble.js
+++ includes/js/spc_bubble.js
@@ -140,68 +140,13 @@ console.log("Bubble");
         }
     }
 
-// Begin Stack overflow code to disable scroll
-    // left: 37, up: 38, right: 39, down: 40,
-    // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-    var keys = {37: 1, 38: 1, 39: 1, 40: 1};
-
-    function preventDefault(e) {
-        console.log(e.target.id);
-        e.preventDefault();
-    }
-
-    function preventDefaultForScrollKeys(e) {
-        if (keys[e.keyCode]) {
-            preventDefault(e);
-            return false;
-        }
-    }
-
-    // modern Chrome requires { passive: false } when adding event
-    var supportsPassive = false;
-    try {
-        window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-            get: function () { supportsPassive = true; } 
-        }));
-    } catch(e) {}
-
-    var wheelOpt = supportsPassive ? { passive: false } : false;
-    var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
-
-    function stopPropagation(e) {
-        e.stopPropagation();
-    }
-
-    // call this to Disable
     function disableScroll() {
-        window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-        window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-        window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-        window.addEventListener('keydown', preventDefaultForScrollKeys, false);
-
-        //But don't use the above events for the modal's content
-        const modal = document.querySelector("#mass-times-modal .content");
-        modal.addEventListener('DOMMouseScroll', stopPropagation, false); // older FF
-        modal.addEventListener(wheelEvent, stopPropagation, wheelOpt); // modern desktop
-        modal.addEventListener('touchmove', stopPropagation, wheelOpt); // mobile
-        modal.addEventListener('keydown', stopPropagation, false);
+        document.body.parentElement.style.overflow = "hidden";
     }
 
-    // call this to Enable
     function enableScroll() {
-        window.removeEventListener('DOMMouseScroll', preventDefault, false);
-        window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
-        window.removeEventListener('touchmove', preventDefault, wheelOpt);
-        window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
-
-        const modal = document.querySelector("#mass-times-modal .content");
-        modal.removeEventListener('DOMMouseScroll', stopPropagation, false); // older FF
-        modal.removeEventListener(wheelEvent, stopPropagation, wheelOpt); // modern desktop
-        modal.removeEventListener('touchmove', stopPropagation, wheelOpt); // mobile
-        modal.removeEventListener('keydown', stopPropagation, false);
+        document.body.parentElement.style.overflow = "";
     }
-// End Stack overflow code to disable scroll
-
 
     injectCss();
     createBubble();
